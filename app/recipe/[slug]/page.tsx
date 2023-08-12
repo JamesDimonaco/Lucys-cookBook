498e4d6ade53f8293aeb89f36087af7697e142b8
James <james@whereyouat.co.uk>
2023-08-12 21:48:02 +0100

    update path to old path

--- app/recipe/[slug]/page.tsx
+++ app/recipe/[slug]/page.tsx
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma";
-import EditRecipe from "../../components/EditRecipe";
+import EditRecipe from "@/app/components/editRecipe";
 import { FullRecipeTypeFromPrisma } from "../../types/recipe";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
